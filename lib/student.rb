1d1b021225b6d4ce8fb9ae5f5253d3eb8519e36d
Angela Appiah <alegnalawn2@yahoo.com>
2020-05-14 18:18:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,6 +33,10 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.save
+    
+  end
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
