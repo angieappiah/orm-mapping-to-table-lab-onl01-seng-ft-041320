2287284583e1e8e55e9cfe87fbbf697da745a374
Angela Appiah <alegnalawn2@yahoo.com>
2020-05-14 18:17:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,9 +26,10 @@ class Student
   end
 
   def self.drop_table
-    sql = <<-SQL DROP TABLE students
+    sql = <<-SQL
+     DROP TABLE students
     SQL
-    
+
     DB[:conn].execute(sql)
   end
 
